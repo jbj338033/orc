9d3ca853387e42b181bb7f947045a9b8eb9e1311
jbj338033 <jbj338033@gmail.com>
2026-03-27 14:01:44 +0900

    feat: add pty backend for terminal

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,15 @@
+mod pty;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .manage(pty::PtyState::new())
+        .invoke_handler(tauri::generate_handler![
+            pty::pty_spawn,
+            pty::pty_write,
+            pty::pty_resize,
+            pty::pty_kill,
+        ])
+        .run(tauri::generate_context!())
+        .expect("failed to run orc");
+}
